e3c2eb3774a4226353fbae1dd003c577902b8bc1
MJ Fang <fmjsjx@163.com>
2026-02-25 15:25:34 +0800

    [Chore] Bump LibCommmon to 4.1.2

--- buildSrc/src/main/kotlin/bson-model3.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/bson-model3.java-library-conventions.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     // libcommon-bom
-    api(platform("com.github.fmjsjx:libcommon-bom:4.1.1"))
+    api(platform("com.github.fmjsjx:libcommon-bom:4.1.2"))
     // mongodb-driver-bom
     api(platform("org.mongodb:mongodb-driver-bom:5.6.3"))
     // log4j2
